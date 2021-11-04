92395eb0cf196e37d47a9eeb0cde8774178e0310
kshkliar <k.shkliar@gmail.com>
2021-11-04 19:12:47 +0200

    the damp car u asked for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        car newcar = new car();
-        newcar.name = "Ferari";
-        newcar.color = "blue";
-        System.out.println(newcar.color);
-        //если хотим отложить на этом месте этот вариант то пишем -git stash -m "ferrari blue color"
+
+        car newcar= new car();
+        newcar.name="Lamo";
+        newcar.color="Red";
+
     }
 }
